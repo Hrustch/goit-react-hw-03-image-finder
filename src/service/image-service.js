8fc3f7f5e69c81cd648edcb33c9a26a70078fa20
Hrustch <nik.stalker47@gmail.com>
2023-06-03 20:07:22 +0300

    fixes

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '36259510-7240acb947e1545cb3b70765a';
-axios.defaults.baseURL = 'http://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 axios.defaults.params = {
   image_type: 'photo',
   orientation: 'horizontal',
